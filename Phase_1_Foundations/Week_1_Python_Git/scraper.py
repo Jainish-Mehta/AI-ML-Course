d69a82c102daa7b2721bd464ae2e2e6a2fb771a0
Jainish-Mehta <jainishmehta2005@outlook.com>
2026-05-27 23:32:31 +0530

    Added a JokeFetcher for Jokes

---
+++ Phase_1_Foundations/Week_1_Python_Git/scraper.py
@@ -0,0 +1,26 @@
+import requests
+
+class JokeFetcher:
+    
+    def __init__(self, name):
+        self.username = name
+        self.api_url = "https://official-joke-api.appspot.com/random_joke"
+        print(f"System Initialized. Welcome, {self.username}!")
+        
+    def get_joke(self):
+        print("Fetching a joke for you...")
+        
+        response = requests.get(self.api_url)
+        
+        if response.status_code == 200:
+            data = response.json()
+            print("\n--- Here's a joke for you ---")
+            print(f"Setup: {data['setup']}")
+            print(f"Punchline: {data['punchline']}")
+            print("-------------------------------")
+        else:
+            print("Failed to Fetch Joke")
+            
+if __name__ == "__main__":
+    my_joke = JokeFetcher(name='ABCD')
+    my_joke.get_joke()
